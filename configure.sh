2585b99bf9a53cb98b3ae6d8fc41e4f32d631768
Carreira Linux <carreiralinux@gmail.com>
2018-06-17 10:33:06 -0300

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,32 @@
+./configure --prefix="/usr" \
+--libexecdir="/usr/libexec/squid" \
+--sysconfdir="/etc/squid" \
+--localstatedir="/var" \
+--libdir="/usr/lib64/squid" \
+--datarootdir="/usr/share/squid" \
+--mandir="/usr/man/squid" \
+--docdir="/usr/doc/squid" \
+--enable-auto-locale \
+--enable-translation \
+--enable-disk-io \
+--enable-removal-policies="heap,lru" \
+--enable-icmp \
+--enable-ipv6 \
+--enable-delay-pools \
+--enable-cache-digests \
+--enable-linux-netfilter \
+--enable-snmp \
+--enable-auth \
+--enable-auth-basic="DB,LDAP,NCSA,SMB,SMB_LM" \
+--enable-auth-digest="LDAP,file" \
+--enable-log-daemon-helpers="DB,file" \
+--enable-external-acl-helpers="LDAP_group,session,unix_group,wbinfo_group" \
+--enable-err-languages="Portuguese" \
+--enable-default-err-language="Portuguese" \
+--with-default-user="squid" \
+--with-logdir="/var/log/squid" \
+--with-pidfile="/var/run/squid.pid" \
+--with-swapdir="/var/cache/squid" \
+--without-netfilter-conntrack \
+--with-filedescriptors="65536" \
+--libdir="/usr/lib64"
---
+++ jre.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export JAVA_HOME=/usr/local/jre-9.0.4
+export MANPATH="${MANPATH}:${JAVA_HOME}/man"
+export PATH="${PATH}:${JAVA_HOME}/bin"
