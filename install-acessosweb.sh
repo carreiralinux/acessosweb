3bf61b09c9ebf9d7f0864bd8f49ca02a50ed6998
Carreira Linux <carreiralinux@gmail.com>
2019-09-19 11:15:00 -0300

    Add files via upload

--- install-acessosweb.sh
+++ install-acessosweb.sh
@@ -204,14 +204,14 @@ read -p "Escolha uma alternativa: " OPCAO
 if [ $OPCAO -eq 2 ];then 
     exit
 else
-        #update_start
-	#install_start
-	#database_start
+        update_start
+	install_start
+	database_start
 	squidguard_start
 	squid_start
-	#jre_start
-        #tomcat_start
-	#firewall_start
-	#wpad_start
+	jre_start
+        tomcat_start
+	firewall_start
+	wpad_start
         msg_start
 fi
