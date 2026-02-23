9fc6843ba3c2054d7a5f03f59639dca01b8bda6e
Carreira Linux <carreiralinux@gmail.com>
2026-02-23 20:06:34 -0300

    Update jre.sh
    20260223 - Atualizado o script para a distribuição Linux Ubuntu Server

--- jre.sh
+++ jre.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-export JAVA_HOME=/usr/lib64/java
+export CATALINA_PID="/opt/tomcat/temp/tomcat.pid"
+export CATALINA_HOME="/opt/tomcat/"
+export CATALINA_BASE="/opt/tomcat/"
+export JAVA_HOME="/usr/lib64/java"
 export MANPATH="${MANPATH}:${JAVA_HOME}/man"
 export PATH="${PATH}:${JAVA_HOME}/bin"
