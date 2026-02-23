7f4e143f9e3e3e47a4f852639854c108dfda6a03
Carreira Linux <carreiralinux@gmail.com>
2026-02-23 20:00:20 -0300

    Update configure.sh
    20260223 - Atualizado script para distribuição Linux Ubuntu Server.

--- configure.sh
+++ configure.sh
@@ -1,18 +1,19 @@
-/usr/src/squid-3.5.22/configure --prefix="/usr" \
---libexecdir="/usr/libexec/squid" \
+/usr/src/squid-6.12/configure --prefix="/usr" \
+--libexecdir="/usr/lib/squid" \
+--datadir="/usr/share/squid" \
 --sysconfdir="/etc/squid" \
 --localstatedir="/var" \
 --libdir="/usr/lib64/squid" \
 --datarootdir="/usr/share/squid" \
 --mandir="/usr/man/squid" \
 --docdir="/usr/doc/squid" \
+--enable-ssl \
 --enable-ssl-crtd \
 --enable-auto-locale \
 --enable-translation \
 --enable-disk-io \
 --enable-removal-policies="heap,lru" \
 --enable-icmp \
---enable-ipv6 \
 --enable-delay-pools \
 --enable-cache-digests \
 --enable-linux-netfilter \
@@ -22,12 +23,13 @@
 --enable-auth-digest="LDAP,file" \
 --enable-log-daemon-helpers="DB,file" \
 --enable-external-acl-helpers="LDAP_group,session,unix_group,wbinfo_group" \
---enable-err-languages="Portuguese" \
---enable-default-err-language="Portuguese" \
+--enable-auto-locale \
+--enable-err-languages="pt-br" \
+--enable-default-err-language="pt-br" \
 --with-openssl \
 --with-default-user="squid" \
 --with-logdir="/var/log/squid" \
---with-pidfile="/var/run/squid.pid" \
+--with-pidfile="/usr/local/squid/squid.pid" \
 --with-swapdir="/var/cache/squid" \
 --without-netfilter-conntrack \
 --with-filedescriptors="65536" \
