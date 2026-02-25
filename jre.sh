01b1bea576ae685ef66d42cab43a845b21ffd020
Carreira Linux <carreiralinux@gmail.com>
2026-02-25 12:05:10 -0300

    Update jre.sh

--- jre.sh
+++ jre.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-export CATALINA_PID="/opt/tomcat/temp/tomcat.pid"
-export CATALINA_HOME="/opt/tomcat/"
-export CATALINA_BASE="/opt/tomcat/"
+export CATALINA_PID="/home/tomcat/temp/tomcat.pid"
+export CATALINA_HOME="/home/tomcat/"
+export CATALINA_BASE="/home/tomcat/"
 export JAVA_HOME="/usr/lib64/java"
 export MANPATH="${MANPATH}:${JAVA_HOME}/man"
 export PATH="${PATH}:${JAVA_HOME}/bin"
